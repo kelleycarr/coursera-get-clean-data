6a72040fda11df127f07545722eb65f85f3856be
Kelley Carr <kelley@hapnyn.com>
2017-02-15 13:38:37 -0800

    Post-code, pre-writeup commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# coursera-get-clean-data
+# Final Project
 Final project for the "Getting and Cleaning Data" course from Coursera
+
+To get this to run, set your working directory to the repository directory. Source run_analysis.R `source("run_analysis.R")` and run the function `run_analysis()`. This function will download and unzip the files required for the project and then perform the data manipulation.
+
+# Explanation of Data Choices
+
+There has been some discussion on whether to include 
---
+++ run_analysis.R
@@ -0,0 +1,80 @@
+# This assumes that your working directory is the repo. 
+# This file needs to be sourced in order for the analysis to run properly.
+# source("run_analysis.R")
+run_analysis <- function() {
+  
+  if (!file.exists("UCI HAR Dataset")) {
+    unzip_data_files()
+  }
+  data_file_path <- file.path(".","UCI HAR Dataset")
+  
+  ## Merge the training and the test sets to create one data set.
+  ## Appropriately label the data set with descriptive variable names.
+  all_data <- merge_sets(data_file_path)
+  
+  ## Extract only the measurements on the mean and standard deviation for each measurement.
+  extracted_data <- all_data[ , grepl("subject_data|y_data|mean|std|Mean", colnames(all_data))]
+  
+  ## Use descriptive activity names to name the activities in the data set
+  extracted_data <- assign_activity_names(extracted_data, data_file_path)
+
+  ## Create an independent tidy data set with the average of each variable for each 
+  ## activity and each subject.
+  tidy_data_summary <- aggregate(. ~ subject_data + activity_label, extracted_data, mean)
+}
+
+unzip_data_files <- function() {
+  # Checks for zip file. If it does not exist, downloads zip file. Unzips file.
+  if (!file.exists("dataset.zip")) {
+    print("Downloading file")
+    download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip",
+                  "dataset.zip")
+  }
+  print("Unzipping file")
+  unzip("dataset.zip")
+}
+
+merge_sets <- function(data_file_path) {
+  features <- read.table(file.path(data_file_path, "features.txt"))
+  
+  test_data <- merge_separate_sets(data_file_path, "test", features)
+  train_data <- merge_separate_sets(data_file_path, "train", features)
+  
+  all_data <- rbind(test_data, train_data)
+  
+  return(all_data)
+}
+
+merge_separate_sets <- function(data_file_path, file_type, features) {
+  file_path <- file.path(data_file_path, file_type)
+  file_list <- list.files(file_path, pattern = ".txt", full.names = TRUE)
+  
+  parse_data <- function(pattern, file_list) {
+    # Searches the list of files for the filename to be read in.
+    # TODO: This will break if multiple files are found.
+    data_file <- file_list[grep(pattern, file_list)]
+    data_points <- read.table(data_file)
+    return(data_points)
+  }
+  
+  subject_data <- parse_data("subject_", file_list)
+  x_data <- parse_data("X_", file_list)
+  y_data <- parse_data("y_", file_list)
+  
+  return_data <- cbind(subject_data, 
+                       y_data, 
+                       x_data)
+  
+  # Set the name of the columns to the "features" list
+  colnames(return_data) <- c("subject_data","activity_code",as.character(features[ , 2]))
+  
+  return(return_data)
+}
+
+assign_activity_names <- function(extracted_data, data_file_path) {
+  activity_labels <- read.table(file.path(data_file_path,"activity_labels.txt"))
+  colnames(activity_labels) <- c("activity_code","activity_label")
+  extracted_data <- merge(extracted_data, activity_labels)
+  extracted_data$activity_code <- NULL
+  return(extracted_data)
+}
